d14026d5baef1fe6ef1de91696a8339f3fec45ff
= <=>
2013-12-03 00:16:22 -0200

    Ajuste na apresentação dos profiles / inserção do fontello

--- public_html/js/fluid.js
+++ public_html/js/fluid.js
@@ -3,12 +3,10 @@ function adjustVisual(){
     var winH = window.innerHeight;
     
     var paper = document.getElementsByClassName('paper')[0];
-    var copyright = document.getElementById('copyright');
     
     //Largura do "papel"
     paper.style.width = ((winW - 90) < 850 ? (winW - 90) : 850)+ "px"
     
     //Margem à esquerda
     paper.style.left = ((winW / 2) - (paper.offsetWidth / 2)) + "px";
-    copyright.style.left = ((paper.offsetWidth / 2) - (copyright.offsetWidth / 2)) + "px";
 }
