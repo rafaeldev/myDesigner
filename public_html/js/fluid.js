3dcf0a8ce8d431215cd5e56848a03686639e9217
= <=>
2013-12-05 23:30:13 -0200

    Ajuste no comportamento de altura da div paper / Ajuste na apresentação dos logos da imagem do profile (mini_me)

--- public_html/js/fluid.js
+++ public_html/js/fluid.js
@@ -1,11 +1,25 @@
-function adjustVisual(){
+function adjustVisualInit(){
     var winW = window.innerWidth;
     var winH = window.innerHeight;
     
     var paper = document.getElementsByClassName('paper')[0];
     
     //Largura do "papel"
-    paper.style.width = ((winW - 90) < 850 ? (winW - 90) : 850)+ "px"
+    paper.style.width = ((winW - 90) < 850 ? (winW - 90) : 850)+ "px";
+    //Altura do "papel"
+    paper.style.minHeight = (winH - 90) + "px";
+    
+    //Margem à esquerda
+    paper.style.left = ((winW / 2) - (paper.offsetWidth / 2)) + "px";
+}
+
+function adjustVisual(){
+    var winW = window.innerWidth;
+    
+    var paper = document.getElementsByClassName('paper')[0];
+    
+    //Largura do "papel"
+    paper.style.width = ((winW - 90) < 850 ? (winW - 90) : 850)+ "px";
     
     //Margem à esquerda
     paper.style.left = ((winW / 2) - (paper.offsetWidth / 2)) + "px";
