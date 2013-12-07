5d83f2619e74cad23df2d2fc444fa1fb7864db71
= <=>
2013-12-07 01:01:27 -0200

    criação da apresentação do author

--- public_html/js/fluid.js
+++ public_html/js/fluid.js
@@ -3,6 +3,7 @@ function adjustVisualInit(){
     var winH = window.innerHeight;
     
     var paper = document.getElementsByClassName('paper')[0];
+    var author = document.getElementById('author');
     
     //Largura do "papel"
     paper.style.width = ((winW - 90) < 850 ? (winW - 90) : 850)+ "px";
@@ -11,6 +12,7 @@ function adjustVisualInit(){
     
     //Margem à esquerda
     paper.style.left = ((winW / 2) - (paper.offsetWidth / 2)) + "px";
+    author.style.left = ((winW / 2) - (author.offsetWidth / 2)) + "px";
 }
 
 function adjustVisual(){
